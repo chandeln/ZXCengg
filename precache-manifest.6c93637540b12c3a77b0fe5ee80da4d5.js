c230eb1b97a887bf2fa4e9adfc5aa21512b10c1d
chandeln <singh.nishant0344@gmail.com>
2022-08-16 20:06:57 +0530

    Updates

---
+++ precache-manifest.6c93637540b12c3a77b0fe5ee80da4d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2e1c2953f28c422a83d9c066a086fc",
+    "url": "/ZXCengg/index.html"
+  },
+  {
+    "revision": "8218dede10721423921c",
+    "url": "/ZXCengg/static/css/main.26dbde14.chunk.css"
+  },
+  {
+    "revision": "5785eee5b631260cebfa",
+    "url": "/ZXCengg/static/js/2.bd93a448.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/ZXCengg/static/js/2.bd93a448.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8218dede10721423921c",
+    "url": "/ZXCengg/static/js/main.02338297.chunk.js"
+  },
+  {
+    "revision": "6cc2c3a1115a209d7425",
+    "url": "/ZXCengg/static/js/runtime-main.7bc08b9a.js"
+  },
+  {
+    "revision": "38b37f9ac62524019719983b2bff0c90",
+    "url": "/ZXCengg/static/media/brands-people-Ax8IA8GAjVg-unsplash.38b37f9a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ZXCengg/static/media/logo.5d5d9eef.svg"
+  }
+]);
